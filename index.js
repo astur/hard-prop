6498d606603231f1abe948056ae948719afa9f2d
astur <astur@yandex.ru>
2016-07-08 02:40:15 +0300

    Parameters validation

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ module.exports = function(object){
 
         var name = args.shift();
 
+        if (typeof name !== 'string') {throw new Error('name must be a string');}
+
         var ad = {
             get: function(){throw new Error('unable to get property: ' + name);},
             set: function(v){throw new Error('unable to set property: ' + name);}
         };
+
+        if (args.length === 0) {throw new Error('need getter or setter function');}
 
         var f = args.shift();
 
         if (typeof f === 'object') {
             ad.get = f.get || ad.get;
             ad.set = f.set || ad.set;
+            if (args.length > 0) {throw new Error('too many arguments');}
         }
 
         while (typeof f === 'function') {
             ad[f.length === 0 ? 'get' : 'set'] = f;
             f = args.shift();
         }
+
+        if (typeof f !== 'undefined') {throw new Error('bad argument');}
 
         Object.defineProperty(this, name, ad);
     };
