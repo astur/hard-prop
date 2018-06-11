9cc2588c53c8a0f32c2358b99a94eaaa7e6ccd56
astur <astur@yandex.ru>
2018-06-11 18:40:08 +0300

    tests reformatted

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
+/* eslint max-statements-per-line: "off" */
 const test = require('ava');
 const hp = require('.');
 
 test('base', t => {
     const obj = {};
     const p = hp(obj);
-    p(
-        'x',
-        () => obj.a,
-        x => { obj.a = x; }
-    );
+    p('x', () => obj.a, x => { obj.a = x; });
     t.deepEqual(obj, {});
     t.is(obj.x, undefined);
     obj.x = 1;
@@ -19,11 +16,7 @@ test('base', t => {
 test('descriptor', t => {
     const obj = {};
     const p = hp(obj);
-    p(
-        'x',
-        () => 1,
-        x => {}
-    );
+    p('x', () => 1, x => {});
     const d = Reflect.getOwnPropertyDescriptor(obj, 'x');
     t.is(typeof d.get, 'function');
     t.is(typeof d.set, 'function');
