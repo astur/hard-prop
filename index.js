55c7048cba510e955d152b1b62488f8b3f71b501
astur <astur@yandex.ru>
2018-06-11 16:52:18 +0300

    refactoring for lint

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 module.exports = function(object){
+    const f = function(...args){
+        const name = args.shift();
 
-    var f = function(){
-        var args = Array.prototype.slice.call(arguments);
+        if(typeof name !== 'string') throw new Error('name must be a string');
 
-        var name = args.shift();
-
-        if (typeof name !== 'string') {throw new Error('name must be a string');}
-
-        var ad = {
-            get: function(){throw new Error('unable to get property: ' + name);},
-            set: function(v){throw new Error('unable to set property: ' + name);}
+        const ad = {
+            get(){ throw new Error(`unable to get property: ${name}`); },
+            set(v){ throw new Error(`unable to set property: ${name}`); },
         };
 
-        if (args.length === 0) {throw new Error('need getter or setter function');}
+        if(args.length === 0) throw new Error('need getter or setter function');
 
-        var f = args.shift();
+        let f = args.shift();
 
-        if (typeof f === 'object') {
+        if(typeof f === 'object'){
             ad.get = f.get || ad.get;
             ad.set = f.set || ad.set;
-            if (args.length > 0) {throw new Error('too many arguments');}
+            if(args.length > 0) throw new Error('too many arguments');
         }
 
-        while (typeof f === 'function') {
+        while(typeof f === 'function'){
             ad[f.length === 0 ? 'get' : 'set'] = f;
             f = args.shift();
         }
 
-        if (typeof f !== 'undefined') {throw new Error('bad argument');}
+        if(typeof f !== 'undefined') throw new Error('bad argument');
 
         Object.defineProperty(this, name, ad);
     };
 
     return f.bind(object);
-
 };
