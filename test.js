bd1dc97b0aadfe3e90d9f5a7182a397f23a2ee0f
astur <astur@yandex.ru>
2018-06-11 18:29:07 +0300

    test for descriptor

--- test.js
+++ test.js
@@ -14,5 +14,19 @@ test('base', t => {
     obj.x = 1;
     t.is(obj.x, 1);
     t.deepEqual(obj, {a: 1});
-    t.pass();
+});
+
+test('descriptor', t => {
+    const obj = {};
+    const p = hp(obj);
+    p(
+        'x',
+        () => 1,
+        x => {}
+    );
+    const d = Reflect.getOwnPropertyDescriptor(obj, 'x');
+    t.is(typeof d.get, 'function');
+    t.is(typeof d.set, 'function');
+    t.is(d.enumerable, false);
+    t.is(d.configurable, false);
 });
